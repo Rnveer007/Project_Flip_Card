e971bd4c353f23eea7b635c8a0fc8cb3895e217f
Rnveer007 <rvs523474@gmail.com>
2024-12-03 16:32:30 +0530

    uodate

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let main_box = document.querySelector(".container");
 let game_btn = document.querySelector(".start_btn");
 let card_back = document.querySelectorAll(".flip-card-back");
 let card_front = document.querySelectorAll(".flip-card-front img");
+let showTime = document.getElementById("dicrTime");
+let flipCard = document.querySelectorAll(".flip-card");
+let result = document.querySelector('.result');
+let showCount = document.querySelector(".showCount");
 let randomImg = [
     "project_imgs/apple.jpg",
     "project_imgs/cherries.webp",
@@ -10,13 +14,14 @@ let randomImg = [
     "project_imgs/orange.jpg",
     "project_imgs/raspberry-2635886_1280.jpg",
 ];
-
 let actualImage = [...randomImg, ...randomImg]
-console.log(actualImage);
-
+let countImg = 0;
+let timer = 60;
 let imgStoreArry = [];
 let tempArry = [];
+let match = 0;
 addImage();
+startTime();
 
 game_btn.addEventListener("click", () => {
     main_box.style.display = "flex";
@@ -33,16 +38,35 @@ function addImage() {
     }
 };
 
-card_front.forEach((frontImage) => {
-    frontImage.addEventListener("click", () => {
-        frontImage.parentElement.parentElement.classList.add(".cardBackSide")
+flipCard.forEach((suffle) => {
+    suffle.addEventListener("click", () => {
+        countImg++;
+        suffle.children[0].classList.add("cardBackSide");
+        imgStoreArry.push(suffle.children[0].children[1].children[0])
+
+        if (countImg === 2) {
+            if (imgStoreArry[0].src === imgStoreArry[1].src) {
+                imgStoreArry.length = 0;
+                countImg = 0;
+                match++
+            }
+            else {
+                setTimeout(() => {
+                    imgStoreArry.forEach((img) => {
+                        img.parentElement.parentElement.classList.remove("cardBackSide");
+                    });
+                    imgStoreArry.length = 0
+                    countImg = 0
+                },1000)
+            }
+        }
     })
-    
+
 });
 
 
 function randomvalue() {
-    let randomItem = Math.random(Math.floor() * actualImage.length);
+    let randomItem = Math.floor(Math.random() * actualImage.length);
     if (tempArry.includes(randomItem)) { return randomvalue() }
     else {
         tempArry.push(randomItem);
@@ -50,5 +74,17 @@ function randomvalue() {
     }
 };
 
+
+
+function startTime(){
+    let interval = setInterval(()=>{
+        showTime.innerHTML = --timer;
+        if(timer === 0){
+            main_box.style.display = "none";
+            result.style.display = "block"
+            clearInterval(interval);
+        }
+    },1000)
+}
 
 
